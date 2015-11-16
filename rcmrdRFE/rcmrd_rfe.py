0242439afd4b10806dae044b660685d6c8e6193b
Bruno Combal <bruno.combal@gmail.com>
2015-11-16 22:56:30 +0300

    business logic, static fnames

--- rcmrdRFE/rcmrd_rfe.py
+++ rcmrdRFE/rcmrd_rfe.py
@@ -20,10 +20,16 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QFile, QFileInfo
+from PyQt4.QtGui import QAction, QIcon, QFileDialog
+from qgis.core import *
 # Initialize Qt resources from file resources.py
 import resources
+import string
+import numpy
+# gdal
+from osgeo import gdal
+from osgeo.gdalconst import *
 # Import the code for the dialog
 from rcmrd_rfe_dialog import rcmrdRFEDialog
 import os.path
@@ -68,6 +74,17 @@ class rcmrdRFE:
         self.toolbar = self.iface.addToolBar(u'rcmrdRFE')
         self.toolbar.setObjectName(u'rcmrdRFE')
 
+        # default values
+        self.inDir='/Users/bruno/Desktop/mesa_data/input/Rainfall'
+        self.dateStart={'day':1,'month':2,'year':2015}
+        self.dateEnd={'day':1,'month':10,'year':2015}
+
+        # filename convention
+        self.fname={'prefix':'', 'suffix':'_rfe.tif'}
+
+        # outfname
+        self.outfile={'outDir':'/Users/bruno/Desktop/mesa_data/output/','rfe':'rfe.tif','rfd':'rfd.tif','rfi':'rfi.tif'}
+        
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
@@ -178,9 +195,155 @@ class rcmrdRFE:
         # remove the toolbar
         del self.toolbar
 
+    # ___________________
+    def logMsg(self, msg, errorLvl=QgsMessageLog.INFO):
+        QgsMessageLog.logMessage(msg, tag='Rainfall Erosivity',level=errorLvl)
+        
+    # ___________________
+    def ParseType(self, type):
+        if type == 'Byte':
+	    return GDT_Byte
+        elif type == 'Int16':
+	    return GDT_Int16
+        elif type == 'UInt16':
+	    return GDT_UInt16
+        elif type == 'Int32':
+	    return GDT_Int32
+        elif type == 'UInt32':
+	    return GDT_UInt32
+        elif type == 'Float32':
+	    return GDT_Float32
+        elif type == 'Float64':
+	    return GDT_Float64
+        elif type == 'CInt16':
+	    return GDT_CInt16
+        elif type == 'CInt32':
+	    return GDT_CInt32
+        elif type == 'CFloat32':
+	    return GDT_CFloat32
+        elif type == 'CFloat64':
+	    return GDT_CFloat64
+        else:
+	    return GDT_Float32
 
+    # ___________________
+    def YYYYMMDD_to_Num(self, yyyymmdd):
+        # base set to 1900
+        yyymmddStr = str(yyyymmdd)
+        thisYear=int(yyyymmdd[0:4])
+        thisMonth=int(yyyymmdd[4:6])
+        thisDay=int(yyyymmdd[6:8])
+
+        return (thisYear-1900)*36 + (thisMonth-1)*3 + (thisDay)/10 + 1
+    # ___________________
+    def Num_to_YYYYMMDD(self, count):
+        # base set to 1900
+        thisYear = count/36
+        thisMonth = (count - (36 * thisYear))/3
+        thisDay = (count - (36 * thisYear) - (3 * thisMonth))*10 + 1
+        thisMonth += 1
+        thisYear += 1900
+
+        return '{}{:02}{:02}'.format(thisYear, thisMonth, thisDay)
+    # ___________________
+    def doCompute(self):
+        # build list of filenames
+        list_dates=[]
+        list_files=[]
+        numStart = self.YYYYMMDD_to_Num('{}{:02}{:02}'.format(self.dateStart['year'],self.dateStart['month'],self.dateStart['day']))
+        numEnd = self.YYYYMMDD_to_Num( '{}{:02}{:02}'.format(self.dateEnd['year'], self.dateEnd['month'], self.dateEnd['day']) )
+
+        for icount in range(numStart, numEnd + 1):
+            thisDate = self.Num_to_YYYYMMDD(icount)
+            self.logMsg('{} -> {}'.format(icount, thisDate))
+            list_dates.append(thisDate)
+            list_files.append( os.path.join(self.inDir, '{}{}{}'.format(self.fname['prefix'], thisDate,self.fname['suffix'])) )
+
+        if len(list_files)==0:
+            # error message
+            exit
+            
+        test=True
+        for ii in list_files:
+            if not os.path.isfile(ii):
+                test=False
+                self.logMsg("Missing file: {}".format(ii) )
+        # error message and exit
+        if test==False:
+            self.logMsg("Time series cannot be processed, missing files. Exit.")
+            exit
+
+        # let's open all files
+        listFID=[]
+        for ii in list_files:
+            self.logMsg(ii)
+            thisFID = gdal.Open(ii, GA_ReadOnly)
+            listFID.append(thisFID)
+        # let's instantiate the output
+        thisProj = listFID[0].GetProjection()
+        thisTrans = listFID[0].GetGeoTransform()
+        ns = listFID[0].RasterXSize
+        nl = listFID[0].RasterYSize
+        format='GTiff'
+        options=['compress=lzw']
+        outType=GDT_Float32
+        outDrv = gdal.GetDriverByName(format)
+        self.logMsg( os.path.join(self.outfile['outDir'],self.outfile['rfe']))
+        outDs = outDrv.Create(os.path.join(self.outfile['outDir'],self.outfile['rfe']),
+                              ns, nl, 1, outType, options  )
+        outDsRFD = outDrv.Create( os.path.join( self.outfile['outDir'], self.outfile['rfd']),
+                                  ns, nl, 1, outType, options)
+        outDsRFI = outDrv.Create( os.path.join( self.outfile['outDir'], self.outfile['rfi']),
+                                  ns, nl, 1, outType, options)
+        outDs.SetProjection(thisProj)
+        outDs.SetGeoTransform(thisTrans)
+        outDsRFD.SetProjection(thisProj)
+        outDsRFD.SetGeoTransform(thisTrans)
+        outDsRFI.SetProjection(thisProj)
+        outDsRFI.SetGeoTransform(thisTrans)
+
+        data=[]
+
+        # parse by line
+        for il in range(nl):
+            data=[]
+            # get the whole time series for this line
+            for ifile in listFID:
+                thisDataset = numpy.ravel(ifile.GetRasterBand(1).ReadAsArray(0, il, ns, 1).astype(float))
+                data.append(thisDataset)
+                
+            data = numpy.asarray(data)
+            sumDepth = data.sum(axis=0)
+
+            # now we had values >= 40mm: let's put all data < 40mm to 0
+            wLow = data < 40
+            if wLow.any():
+                data[wLow] = 0
+            sumIntensity = data.sum(axis=0)
+            
+            sumErosivity = self.dlg.valueWRI.value()*sumIntensity + self.dlg.valueWRD.value()* sumDepth
+
+            # write outputs
+            self.logMsg('size {}'.format(sumErosivity.shape))
+            self.logMsg('ns {}, nl {}'.format(ns, nl))
+            outDs.GetRasterBand(1).WriteArray(sumErosivity.reshape(1,ns), 0, il)
+            outDsRFI.GetRasterBand(1).WriteArray(sumIntensity.reshape(1,ns),0,il )
+            outDsRFD.GetRasterBand(1).WriteArray(sumDepth.reshape(1,ns),0,il)
+
+        # close files
+        for ii in listFID:
+            ii = None
+
+        return True
+    # ___________________
+    def doInitGui(self):
+        self.dlg.editInputDir.clear()
+        self.dlg.editInputDir.setText( self.inDir )
+
+    # ____________________
     def run(self):
-        """Run method that performs all the real work"""
+        # initialise GUI
+        self.doInitGui()
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
@@ -189,4 +352,6 @@ class rcmrdRFE:
         if result:
             # Do something useful here - delete the line containing pass and
             # substitute with your code.
-            pass
+            self.doCompute()
+
+            
