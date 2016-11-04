1beed858803d6d313ca671890d2394c8d80c245d
brunocombal <bruno.combal@gmail.com>
2016-11-04 16:35:59 +0100

    added functions to burn shapefile and count values per polygon ID

--- FireStats/fireStatsTools.py
+++ FireStats/fireStatsTools.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 #from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QFile, QFileInfo
 #from qgis.core import *
+import numpy
+# gdal
+from osgeo import gdal, ogr
+from osgeo.gdalconst import *
 import os, os.path
+ogr.UseExceptions(True)
 
 # __________
 # ref.: http://pythoncentral.io/pyside-pyqt-tutorial-the-qlistwidget/
@@ -9,3 +14,89 @@ def updateBulletinList(widgetList):
 	for ii in range(100):
 		widgetList.addItem("Coucou {}".format(ii))
 
+
+# __________
+def ParseType(self, type):
+	if type == 'Byte':
+    	return GDT_Byte
+    elif type == 'Int16':
+    	return GDT_Int16
+    elif type == 'UInt16':
+    	return GDT_UInt16
+    elif type == 'Int32':
+    	return GDT_Int32
+    elif type == 'UInt32':
+    	return GDT_UInt32
+    elif type == 'Float32':
+    	return GDT_Float32
+    elif type == 'Float64':
+    	return GDT_Float64
+    elif type == 'CInt16':
+    	return GDT_CInt16
+    elif type == 'CInt32':
+    	return GDT_CInt32
+    elif type == 'CFloat32':
+    	return GDT_CFloat32
+    elif type == 'CFloat64':
+    	return GDT_CFloat64
+    else:
+    	return GDT_Float32
+
+# __________
+def createTmpDir(conf):
+	if not os.path.exists(conf["tmp_dir"]):
+    	os.makedirs(conf["tmp_dir"])
+
+# __________
+# Input: a shapefile, with polygon; each polygon as an id
+# Input: a reference raster, will be copied to blank for burning the shapefile
+def doBurnShpToRaster(shpFile, refRaster, outRaster):
+	ds = gdal.Open(refRaster, GA_ReadOnly)
+	cols = ds.RasterXSize
+	rows = ds.RasterYSize
+	projection = ds.GetProjection()
+	geotransform = ds.GetGeoTransform()
+	bands = 1
+	datatype = GDT_Int32
+
+	#neatline = ds.GetMetadata()['NEATLINE'] 
+
+	#driver = ogr.GetDriverByName('Memory')
+	#poly = ogr.CreateGeometryFromWkt(neatline)
+	#memds = driver.CreateDataSource('tmpmemds')
+	#lyr = memds.CreateLayer('neatline', geom_type=ogr.wkbPolygon)
+	#feat = ogr.Feature(lyr.GetLayerDefn())
+	#feat.SetGeometry(poly)
+	#lyr.CreateFeature(feat)
+	shpDS = ogr.Open(shpFile)
+	lyr = shpDS.GetLayer()
+
+	driver = gdal.GetDriverByName('GTiff')
+	outds = driver.Create(outRaster, cols, rows, bands, datatype, ["compress='LZW'"])
+	outds.SetProjection(projection)
+	outds.SetGeoTransform(geotransform)
+	gdal.RasterizeLayer(outds, [1], lyr, None, None, [1], ['ALL_TOUCHED=TRUE'])
+
+# __________
+# Reference: a raster, pixel value=polygon id (generated with doBurnShpToRaster)
+# Input: a sparse raster with 0=no detection. It must have the same resolution, projection and footprint as the detectionRaster
+# Output: associative array, array[id]=count in the polygon
+def doCountPerPolygonId(refRaster, detectionRaster):
+	countPerId={}
+
+	refFID = gdal.Open(refRaster, GA_ReadOnly)
+	thisFID = gdal.Open(detectionRaster, GA_ReadOnly)
+	ns = thisFID.RasterXSize
+	nl = thisFID.RasterYSize
+
+	for il in range(nl):
+		refLine = numpy.ravel( refFID.GetRasterBand(1).ReadAsArray(0,il,ns,1).astype(Int32) )
+		dataLine = numpy.ravl( thisFID.GetRasterBand(1).ReadAsArray(0, il, ns, 1).astype(int) )
+		indexToSum = numpy.nonzero( dataLine > 0 ) #Returns the indices of the elements that are non-zero.
+		for ii in indexToSum:
+			if refLine[ii] in countPerId: # does the key already exist in countPerId?
+				countPerId[ refLine[ii] ] = countPerId[ refLine[ii] ] + 1
+			else: # else create it
+				countPerId[ refLine[ii] ] = 1
+
+	return countPerId
